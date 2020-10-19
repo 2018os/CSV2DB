942d0dba582b1ceb147f800db3bc04ae78b07d92
seungyeop Kim <seungyeop2002@gmail.com>
2020-10-19 22:30:17 +0900

    Separate code

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-const csv = require("csv-parser");
 const { PrismaClient } = require("@prisma/client");
-const fs = require("fs");
+
+const { sleep, stringToBoolean, getData } = require("./tools");
 
 const prisma = new PrismaClient();
 
 const AUTHOR_ID_UNIT = 1000;
 const WEBTOON_ID_UNIT = 10000;
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-function stringToBoolean(text) {
-  return text === "TRUE";
-}
-
-function getData(path) {
-  const results = [];
-  const stream = fs.createReadStream(path).pipe(csv());
-  return new Promise((resolve, reject) => {
-    stream.on("data", (data) => {
-      results.push(data);
-    });
-    stream.on("error", (err) => {
-      resolve(reject);
-    });
-    stream.on("end", () => {
-      resolve(results);
-    });
-  });
-}
 
 async function insertGenres(genre) {
   return prisma.genre.create({
@@ -73,25 +49,6 @@ async function insertWebtoons(webtoon) {
     return { id: Buffer.from(String(authorIdaddedUnit)).toString("base64") };
   });
   const webtoonIdaddedUnit = Number(id) + WEBTOON_ID_UNIT;
-  // return prisma.webtoon.create({
-  //   data: {
-  //     id: Buffer.from(String(webtoonIdaddedUnit)).toString("base64"),
-  //     title,
-  //     description,
-  //     isAdult: stringToBoolean(isAdult),
-  //     isFinish: stringToBoolean(isFinish),
-  //     isPay: !stringToBoolean(isFree),
-  //     platform,
-  //     thumbnail,
-  //     url,
-  //     authors: {
-  //       connect: authorObj,
-  //     },
-  //     genres: {
-  //       connect: genreObj,
-  //     },
-  //   },
-  // });
   const data = {
     id: Buffer.from(String(webtoonIdaddedUnit)).toString("base64"),
     title,
@@ -109,24 +66,29 @@ async function insertWebtoons(webtoon) {
       connect: genreObj,
     },
   };
-  // console.dir(data, { depth: null });
   return prisma.webtoon.create({
     data,
   });
 }
 
-async function main() {
-  const trigger = process.argv[2]; // "genre" or "author" or "webtoon"
-  const data = await getData(`csv/${trigger}.csv`);
-  const action =
-    trigger === "genre"
-      ? insertGenres
-      : trigger === "author"
-      ? insertAuthors
-      : insertWebtoons;
+async function executeInsertData(action, path) {
+  const data = await getData(path);
   for (let i = 0; i < data.length; i++) {
     sleep(2000);
     await action(data[i]);
+  }
+}
+async function main() {
+  const trigger = process.argv[2];
+  switch (trigger) {
+    case "genre":
+      return executeInsertData(insertGenres, "csv/genre.csv");
+    case "author":
+      return executeInsertData(insertAuthors, "csv/author.csv");
+    case "webtoon":
+      return executeInsertData(insertWebtoons, "csv/webtoon.csv");
+    default:
+      return 0;
   }
 }
 
